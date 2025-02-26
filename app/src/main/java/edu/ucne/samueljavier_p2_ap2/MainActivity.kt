b48fd2856c737b401a78cb1e112e4dec374cb0b3
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-02-25 21:38:17 -0400

    Initial commit

---
+++ app/src/main/java/edu/ucne/samueljavier_p2_ap2/MainActivity.kt
@@ -0,0 +1,26 @@
+package edu.ucne.samueljavier_p2_ap2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.samueljavier_p2_ap2.ui.theme.SamuelJavier_P2_AP2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SamuelJavier_P2_AP2Theme {
+
+            }
+        }
+    }
+}
