e2cbab03a7d743aa382a4e303498cf1d9be9ca5a
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-02-26 20:23:51 -0400

    Trabajando NavHost

---
+++ app/src/main/java/edu/ucne/samueljavier_p2_ap2/presentation/navigation/NavigationHost.kt
@@ -0,0 +1,46 @@
+package edu.ucne.samueljavier_p2_ap2.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.samueljavier_p2_ap2.presentation.deposito.DepositoListScreen
+import edu.ucne.samueljavier_p2_ap2.presentation.deposito.DepositoScreen
+
+@Composable
+fun AppNavHost(navHostController: NavHostController
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    val scope = rememberCoroutineScope()
+    NavHost(
+
+        navController = navHostController,
+        startDestination = Screen.Home
+    ) {
+
+        composable<Screen.DepositoScreen> {
+            val depositoId = it.toRoute<Screen.DepositoScreen>().depositoId
+            DepositoScreen(
+                depositoId = depositoId,
+                goBackToList = { navHostController.navigateUp() }
+
+            )
+        }
+
+        composable<Screen.DepositoListScreen> {
+            DepositoListScreen(
+                createDeposito = { navHostController.navigate(Screen.DepositoScreen(0)) },
+                goToMenu = { navHostController.navigateUp() },
+                goToDeposito = { depositoId ->
+                    navHostController.navigate(Screen.DepositoScreen(depositoId = depositoId))
+                }
+            )
+
+        }
+    }
+}
